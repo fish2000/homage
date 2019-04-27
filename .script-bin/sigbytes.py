953068d0a05e3738c75e243e7ef10fe7352d1c09
Alexander Böhn <fish2000@users.noreply.github.com>
2019-04-27 03:50:23 -0400

    More updates to sigbytes.py
    * OCD-d out on the `docopt` text
    * Using a custom exception subclass
    * Delegating from a `main(…)` function to `cli(…)`
    * Calling a filehandle a filehandle, not an “fp” (whatever that means)
    * Using a full descriptive version string, vs. just a stringified semver-ish number
    * Logic fix in output path check

--- .script-bin/sigbytes.py
+++ .script-bin/sigbytes.py
@@ -9,21 +9,25 @@
 #
 """
 Usage:
-  sigbytes.py INFILE...       [-o OUTFILE]
-                              [-s SIZE]
-                              [-c | --clean]
-                              [-V | --verbose]
+  sigbytes.py INFILE...  [ -o OUTFILE   | --output=OUTFILE  ]
+                         [ -s SIZE      | --size=SIZE       ]
+                         [ -c           | --clean           ]
+                         [ -V           | --verbose         ]
   
-  sigbytes.py                  -h | --help |
-                               -v | --version
-    
+  sigbytes.py              -h           | --help
+  sigbytes.py              -v           | --version
+
+Arguments:
+  INFILE                                Source file(s) to be read.
+
 Options:
-  -o OUTFILE --output=OUTFILE       specify output file [default: stdout].
-  -s SIZE --size=SIZE               specify size of bytes to read [default: 8].
-  -c --clean                        clean first, strip out the slash-x punctuation.
-  -V --verbose                      print verbose output.
-  -h --help                         show this text.
-  -v --version                      print version.
+  -o OUTFILE --output=OUTFILE           Output file [default: stdout].
+  -s SIZE --size=SIZE                   Size of bytes to read [default: 8].
+  -c --clean                            Clean output: strip slash-x characters.
+  -V --verbose                          Spew verbose output to stdout.
+  -h --help                             Show this text.
+  -v --version                          Print the program version and exit.
+
 """
 
 from __future__ import print_function
@@ -32,26 +36,32 @@ from os.path import exists, isdir, dirname, expanduser
 from docopt import docopt
 import sys, re
 
+class ArgumentError(ValueError):
+    """ An issue with the supplied arguments """
+    pass
+
+VERSION = u'sigbytes.py 0.2.0 © 2015-2019 Alexander Böhn / OST, LLC'
+
 def cli(argv=None):
     if not argv:
         argv = sys.argv
     
     arguments = docopt(__doc__, argv=argv[1:],
                                 help=True,
-                                version='0.2.0')
+                                version=VERSION)
     
     # print(argv)
     # print(arguments)
     # sys.exit()
+    
+    if not len(arguments.get('INFILE')) > 0:
+        raise ArgumentError("No input files")
     
     ipths = (expanduser(pth) for pth in arguments.get('INFILE'))
     opth = expanduser(arguments.get('--output'))
     siz = int(arguments.get('--size', 8))
     clean = bool(arguments.get('--clean'))
     verbose = bool(arguments.get('--verbose'))
-    
-    if not len(arguments.get('INFILE')) > 0:
-        raise AttributeError("No input files")
     
     cleanr = lambda b: repr(b).replace(r'\x', ' ').upper()
     process_bytes = clean and cleanr or repr
@@ -67,12 +77,12 @@ def cli(argv=None):
         print("")
         print("*** Found %s byte signatures" % len(signatures))
     
-    fp = None
+    filehandle = None
     if opth != 'stdout':
-        if exists(opth) or isdir(dirname(opth)):
-            raise AttributeError("Bad output file")
+        if exists(opth) or not isdir(dirname(opth)):
+            raise ArgumentError("Bad output file")
         else:
-            fp = open(opth, 'wb')
+            filehandle = open(opth, 'wb')
     
     for pth, signature in signatures.items():
         if opth == 'stdout':
@@ -82,15 +92,23 @@ def cli(argv=None):
             else:
                 print(process_bytes(signature)[2:-1].strip())
         else:
-            fp.write(signature)
-            fp.write(b"\n")
+            filehandle.write(signature)
+            filehandle.write(b"\n")
     
-    if fp is not None:
-        fp.flush()
-        fp.close()
+    if filehandle is not None:
+        filehandle.flush()
+        filehandle.close()
     elif verbose:
         print("")
 
+def main():
+    try:
+        cli(sys.argv)
+    except ArgumentError:
+        print("[error] bad arguments passed:",
+              file=sys.stderr)
+        raise
+    sys.exit(0)
 
 if __name__ == '__main__':
-    cli(sys.argv)
+    main()
