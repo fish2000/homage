6067763896a2bd272cecb0277673fd795851f819
Alexander Böhn <fish2000@users.noreply.github.com>
2019-05-31 12:05:28 -0400

    Broke out a few more functions

--- .script-bin/screensize.py
+++ .script-bin/screensize.py
@@ -6,7 +6,9 @@
 #       Return a dictionary with information about the current screen --
 #       resolution, viewable size rectanges, raw pixel dimensions, and
 #       other assorted miscellaneous data and metadata.
-#       Requires PyObjC; ANSI color output requires Pygments and Colorama.
+# 
+#       Requires PyObjC and NumPy; ANSI color output requires Pygments
+#       and Colorama.
 # 
 #       © 2019 Alexander Böhn, All Rights Reserved.
 # 
@@ -18,47 +20,124 @@ Usage:
   screensize.py     -v  |  --version
 
 Options:
-  -D --debug        print human-readable formatted JSON.
-  -A --ansi         print ANSI-highlighted formatted JSON (requires Pygments).
+  -D --debug        pretty-print human-readable JSON.
+  -A --ansi         pretty-print ANSI-highlighted‡ JSON.
   -h --help         exit after showing this help text.
   -v --version      exit after showing this programs’ version.
 
+Environment Variables:
+  DEBUG             set to 1 to enable (same as passing -D/--debug)
+
 Returns:
   JSON-formatted screen information.
+
+Notes:
+  ‡ ANSI syntax highlighting equires Pygments.
 """
 from __future__ import print_function, unicode_literals
+
+objc = None
 
 import json
 import sys
 import os
+import numpy
 
 DEBUG = bool(int(os.environ.get('DEBUG', '0'), base=10))
+
+class CoreGraphicsError(RuntimeError):
+    """ An error was returned from a CoreGraphics function """
+    pass
 
 class DisplayAndExit(SystemExit):
     """ A signal to the caller to exit cleanly """
     pass
+
+def swizzle(cls, SEL, func):
+    """ Pythonic method swizzling! Courtesy pytest-osxnotify:
+            https://github.com/dbader/pytest-osxnotify
+    """
+    old_IMP = getattr(cls, SEL, None)
+    if old_IMP is None:
+        # This will work on OS X <= 10.9
+        old_IMP = cls.instanceMethodForSelector_(SEL)
+    
+    def wrapper(self, *args, **kwargs):
+        return func(self, old_IMP, *args, **kwargs)
+    
+    new_IMP = objc.selector(
+        wrapper,
+        selector=old_IMP.selector,
+        signature=old_IMP.signature
+    )
+    objc.classAddMethod(cls, SEL.encode(), new_IMP)
+
+def swizzled_bundleIdentifier(self, original):
+    return bool(os.environ.get('TM_PYTHON')) and 'com.macromates.TextMate' \
+                                              or 'com.apple.Terminal'
+
+def clamp(value, min=None, max=None):
+    """ Clamp a value within the bounds of an unsigned 8-bit integer """
+    min = min or clamp.info.min
+    max = max or clamp.info.max
+    return numpy.clip(value, a_min=min,
+                             a_max=max)
+
+# Specify the numpy type to use with `clamp(…)`:
+clamp.type = numpy.uint8
+
+# Supply the upper and lower bounds for the `clamp(…)` type:
+clamp.info = numpy.iinfo(clamp.type)
 
 def screensize(*, screen_idx=0):
     """ Return a dict full of information about the “main screen” --
         as per the whim of the return value furnished by a call to
         the `(NSRect*)[NSScreen mainScreen]` method.
         
-        This function currently requires a recent PyObjC.
+        This function currently requires a recent release of PyObjC
     """
+    # Memoize, and possibly swizzle, the `objc` module:
+    global objc
+    if not objc:
+        objc = __import__('objc')
+        swizzle(objc.lookUpClass('NSBundle'),
+                                 'bundleIdentifier',
+                                  swizzled_bundleIdentifier)
+    
     # Import NSScreen from QuartzCore (*not* Foundation):
-    from Quartz.QuartzCore import NSScreen
+    import Quartz
+    import Quartz.QuartzCore
+    _ = dir(Quartz.QuartzCore)
+    NSScreen = objc.lookUpClass('NSScreen')
+    
+    # Get the “active display list” via a CoreGraphics call --
+    # If you have two displays set up to mirror one another,
+    # this function counts them as one thing; for all the things
+    # regardless of mirroring, one calls `CGGetOnlineDisplayList`
+    # instead, with like the same call signature and shit.
+    dmax = clamp.info.max
+    error, screens, screen_count = Quartz.CGGetActiveDisplayList(dmax, None, None)
+    
+    # Error value check:
+    if error:
+        raise CoreGraphicsError("CGGetActiveDisplayList(…) error: %s" % error)
+    
+    # Clamp “screen_idx” argument before indexing with it:
+    screen_idx = clamp(screen_idx, min=0, max=screen_count)
     
     # Pick an NSScreen instance, defaulting to the main screen:
-    screen = (screen_idx > 1) and NSScreen.screens()[screen_idx] \
+    screen = (screen_idx > 0) and NSScreen.screens()[screen_idx] \
                                or NSScreen.mainScreen()
     
-    # Gather dimension data from the main screen:
+    # Gather dimension data from the specified screen,
+    # using the public NSScreen API:
     devpixels = screen.devicePixelCounts()      # NSRect of the raw pixel counts
     visible = screen.visibleFrame()             # NSRect of the “visible” frame (?)
     frame = screen.frame()                      # NSRect of the screen frame
     depth = screen.depth()                      # Not sure what this is actually
     
-    # Gather data from private/undocumented methods:
+    # Gather additional data from the specified screen,
+    # from private and/or undocumented methods:
     settings = screen._currentSetting()         # Giant, miscellaneous info-dump
     displayID = screen._displayID()             # Internal Objective-C target ID
     menuBarHeight = screen._menuBarHeight()     # Height of the menu bar, like duh
@@ -74,22 +153,32 @@ def screensize(*, screen_idx=0):
                           'height' : int(visible.size.height) },
                'frame' : { 'width' : int(frame.size.width),
                           'height' : int(frame.size.height) },
-          'resolution' : { 'width' : int(resolution.width),
-                          'height' : int(resolution.height) },
+          'resolution' : {     'x' : int(resolution.width),
+                               'y' : int(resolution.height) },
             
        'menuBarHeight' : int(menuBarHeight),
                'depth' : int(depth),
            'displayID' : int(displayID),
+         'displayList' : list(screens),
+        'displayCount' : int(screen_count),
             'settings' : dict(settings)
             
     }
+    
+    # Manually release the NSScreen class and instance,
+    # and the settings NSDictionary:
+    del NSScreen
+    del settings
+    del screen
     
     # Return the output dict:
     return screen_info
 
 def to_json(dictionary, **options):
+    """ Format a JSON dictionary as a string, with options """
     return json.dumps(dictionary, **options)
 
+# Set up formatting options:
 to_json.options = {}
 
 to_json.options['debug']   = dict(indent=4,
@@ -111,15 +200,16 @@ def print_color(text, color='', reset=None):
         print(color + line, sep='')
     print(reset or colorama.Style.RESET_ALL, end='')
 
-def highlight(json_string):
-    """ Highlight a given JSON string using inlin ANSI delimiters,
+def highlight(json_string, language='json',
+                             markup='terminal256',
+                              style='paraiso-dark'):
+    """ Highlight a JSON string with inline 256-color ANSI markup,
         using `pygments.highlight(…)` and the “Paraiso Dark” theme
     """
     import pygments, pygments.lexers, pygments.formatters
-    LexerCls = pygments.lexers.find_lexer_class_by_name('json')
-    formatter = pygments.formatters.get_formatter_by_name('terminal256')
-    return pygments.highlight(json_string, formatter=formatter,
-                                           lexer=LexerCls(style='paraiso-dark'))
+    LexerCls = pygments.lexers.find_lexer_class_by_name(language)
+    formatter = pygments.formatters.get_formatter_by_name(markup, style=style)
+    return pygments.highlight(json_string, lexer=LexerCls(), formatter=formatter)
 
 VERSION = u'screensize.py 0.1.0 © 2019 Alexander Böhn / OST, LLC'
 
@@ -138,12 +228,13 @@ def main(argv=None, debug=False):
     if not argv:
         argv = sys.argv
     
+    # Dispatch any print-and-exit args, if present:
     if '--help' in argv or '-h' in argv:
         show_help()
     elif '--version' in argv or '-v' in argv:
         show_version()
     
-    # Deal with arguments:
+    # Deal with normal arguments:
     ansi = '--ansi' in argv or '-A' in argv
     debug = ansi or debug or ('--debug' in argv or '-D' in argv)
     ansi &= not bool(os.environ.get('TM_PYTHON'))
@@ -164,7 +255,10 @@ def main(argv=None, debug=False):
     # All is well, return to zero:
     return 0
 
-__all__ = ('DEBUG', 'VERSION', 'screensize', 'to_json', 'print_color', 'highlight')
+__all__ = ('DEBUG', 'VERSION', 'clamp', 'screensize',
+                               'to_json', 'print_color',
+                               'highlight')
+
 __dir__ = lambda: list(__all__)
 
 if __name__ == '__main__':
