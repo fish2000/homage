f7f4e5dd1313a2ec1f725cd061ed995bc32df907
Alexander Böhn <fish2000@gmail.com>
2019-11-15 14:05:26 -0500

    WHOOOOOOPS

--- .script-bin/async-redis-leftovers.py
+++ .script-bin/async-redis-leftovers.py
@@ -1,98 +1,81 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
-#
-#
-#   get_terminal_size()
-# 
-#   from https://gist.github.com/jtriley/1108174
-#
 from __future__ import print_function
 
-import os
-import shlex
-import struct
-import platform
-import subprocess
+import asyncio
+import concurrent.futures
+import contextvars
+import warnings
 
- 
-def get_terminal_size(default=(80, 25)):
-    """ get_terminal_size()
-     - get width and height of console
-     - works on linux, os x, windows, cygwin/windows
-     originally retrieved from:
-     http://stackoverflow.com/questions/566746/how-to-get-console-window-width-in-python
-    """
-    current_os = platform.system()
-    tuple_xy = None
-    if current_os == 'Windows':
-        tuple_xy = _get_terminal_size_windows()
-        if tuple_xy is None:
-            tuple_xy = _get_terminal_size_tput()
-            # needed for window's python in cygwin's xterm!
-    if current_os in ['Linux', 'Darwin'] or current_os.startswith('CYGWIN'):
-        tuple_xy = _get_terminal_size_linux()
-    if tuple_xy is None:
-        # print("default")
-        tuple_xy = default
-    return tuple_xy
- 
- 
-def _get_terminal_size_windows():
-    try:
-        from ctypes import windll, create_string_buffer
-        # stdin handle is -10
-        # stdout handle is -11
-        # stderr handle is -12
-        h = windll.kernel32.GetStdHandle(-12)
-        csbi = create_string_buffer(22)
-        res = windll.kernel32.GetConsoleScreenBufferInfo(h, csbi)
-        if res:
-            (bufx, bufy, curx, cury, wattr,
-             left, top, right, bottom,
-             maxx, maxy) = struct.unpack("hhhhHhhhhhh", csbi.raw)
-            sizex = right - left + 1
-            sizey = bottom - top + 1
-            return sizex, sizey
-    except:
-        pass
- 
+from clu.naming import nameof
+from clu.fs.filesystem import which
 
-def _get_terminal_size_tput():
-    # get terminal width
-    # src: http://stackoverflow.com/questions/263890/how-do-i-find-the-width-height-of-a-terminal-window
+PID = contextvars.ContextVar('PID')
+
+def redis_server(*args):
+    return asyncio.create_subprocess_exec(
+        which('redis-server'), *args,
+        stdout=asyncio.subprocess.DEVNULL,
+        stderr=asyncio.subprocess.DEVNULL)
+
+async def run_redis_server(*args):
+    print("[daemon] Starting Redis…")
+    daemon = await redis_server(*args)
+    
+    print(f"[daemon] PID = {daemon.pid}")
+    PID.set(daemon.pid)
+    
     try:
-        cols = int(subprocess.check_call(shlex.split('tput cols')))
-        rows = int(subprocess.check_call(shlex.split('tput lines')))
-        return (cols, rows)
-    except:
-        pass
- 
- 
-def _get_terminal_size_linux():
-    def ioctl_GWINSZ(fd):
+        print("[daemon] Awaiting interrupt signal")
+        await daemon.wait()
+    except KeyboardInterrupt:
+        print("[daemon] Terminating process…")
+        daemon.terminate()
+    
+    if daemon.returncode is None:
+        print("[daemon] Killing process…")
+        daemon.kill()
+    
+    print(f"[daemon] RETVAL = {daemon.returncode}")
+
+async def run_redis_task(*args):
+    PID.set(-1)
+    task = asyncio.create_task(run_redis_server(*args))
+    taskID = hex(id(task))
+    pid = PID.get()
+    argstring = ', '.join(args)
+    print(f"[runctx] Redis server task created <id:{taskID}>")
+    print(f"[runctx] PID = {pid}")
+    print(f"[runctx] args = ({argstring})")
+    return await task
+
+def schedule(target, *, loop=None):
+    if asyncio.iscoroutine(target):
+        return asyncio.ensure_future(target, loop=loop)
+    typename = nameof(type(target))
+    raise TypeError(f"target must be a coroutine (not {typename})")
+
+def main(*args):
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
         try:
-            import fcntl
-            import termios
-            cr = struct.unpack('hh',
-                               fcntl.ioctl(fd, termios.TIOCGWINSZ, '1234'))
-            return cr
-        except:
-            pass
-    cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
-    if not cr:
-        try:
-            fd = os.open(os.ctermid(), os.O_RDONLY)
-            cr = ioctl_GWINSZ(fd)
-            os.close(fd)
-        except:
-            pass
-    if not cr:
-        try:
-            cr = (os.environ['LINES'], os.environ['COLUMNS'])
-        except:
-            return None
-    return int(cr[1]), int(cr[0])
- 
-if __name__ == "__main__":
-    sizex, sizey = get_terminal_size()
-    print('width =', sizex, 'height =', sizey)
+            asyncio.run(run_redis_task(*args))
+        except KeyboardInterrupt:
+            print()
+            print("[runctx] Shutting down…")
+
+async def async_background():
+    loop = asyncio.new_event_loop()
+    with concurrent.futures.ProcessPoolExecutor() as pool:
+        result = await loop.run_in_executor(pool, run_redis_task())
+    print('custom process pool', result)
+
+def background():
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        future = executor.submit(main)
+        print(future.result)
+
+if __name__ == '__main__':
+    # main()
+    # asyncio.run(background())
+    background()
