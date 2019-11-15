eb5ea5f404130beb95288891190548d88f2ea25b
Alexander Böhn <fish2000@gmail.com>
2019-11-15 13:48:34 -0500

    REDRUN REDRUN REDRUN:

--- .script-bin/async-redis.py
+++ .script-bin/async-redis.py
@@ -1,22 +1,47 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 from __future__ import print_function
-from distutils.spawn import find_executable
+from pprint import pprint
 
 import asyncio
-import concurrent.futures
 import contextvars
+import logging
 import multidict
 import os
-import warnings
+import signal
 
-# from clu.fs.filesystem import back_tick
-from clu.fs.filesystem import TemporaryName, Directory, TemporaryDirectory
-from clu.naming import nameof
+from clu.constants.consts import DEBUG
+from clu.fs.filesystem import (which,
+                               TemporaryName,
+                               TemporaryDirectory,
+                                        Directory)
+
+'''
+RUNNING REDIS:
+
+1. Relevant command lines:
+    
+    * Detect:
+    • ps ax | grep redis-server | grep -v grep
+    
+    * Kill:
+    • kill -9 `ps ax | grep redis-server | grep -v grep | cut -d " " -f 1,1`
+    
+2. Configuration:
+    
+    
+'''
+
+# logging.getLogger("asyncio").setLevel(DEBUG and logging.DEBUG or logging.WARNING)
+# logg = logging.getLogger("clu.rack.redis")
+# logg.setLevel(logging.WARNING)
+# loggout = logging.StreamHandler()
+# loggout.setLevel(DEBUG and logging.DEBUG or logging.ERROR)
+# logg.addHandler(loggout)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(relativeCreated)6d %(threadName)s %(message)s')
 
 PID = contextvars.ContextVar('PID')
-
-CONF = """cat /usr/local/etc/redis.conf | grep -v -e "^#" | sed '/^[[:space:]]*$/d'"""
 
 class RedisConf(object):
     
@@ -30,14 +55,16 @@ class RedisConf(object):
         self.config = multidict.MultiDict()
         self.port = port
         self.source = source
+        self.active = False
         self.parse(self.source \
            or type(self).DEFAULT_SOURCE)
     
     def parse(self, source):
         with open(source, 'r') as handle:
             lines = filter(None,
+                    map(lambda line: line.strip(),
                     filter(lambda line: not line.startswith('#'),
-                           handle.readlines()))
+                           handle.readlines())))
         for line in lines:
             parts = line.split(None, 1)
             self.config.add(parts[0],
@@ -51,8 +78,9 @@ class RedisConf(object):
         self.config[key] = self.decompose(value)
     
     def set_port(self, port):
+        redisdir = self.get_dir()
         self.set('port',    str(port))
-        self.set('pidfile', f"/var/run/redis_{port}.pid")
+        self.set('pidfile', redisdir.subpath(f"redis_{port}.pid"))
     
     def get_port(self):
         return int(' '.join(self.config.get('port')), base=10)
@@ -84,17 +112,22 @@ class RedisConf(object):
             lines.extend(self.getlines(key))
         return "\n".join(lines)
     
+    @property
+    def path(self):
+        return self.file.name
+    
     def __enter__(self):
         rdir = TemporaryDirectory(prefix="redis-")
         conf = TemporaryName(prefix='redis-config-',
                              suffix='conf',
                              parent=rdir,
                              randomized=True)
-        self.set_port(self.port)
         self.set_dir(rdir)
+        self.set_port(self.port)
         conf.write(self.assemble())
         self.rdir = rdir
         self.file = conf
+        self.active = True
         return self
     
     def __exit__(self, exc_type=None, exc_val=None, exc_tb=None):
@@ -104,6 +137,7 @@ class RedisConf(object):
         if self.rdir:
             self.rdir.close()
             del self.rdir
+        self.active = False
         return exc_type is None
     
     def __repr__(self):
@@ -114,80 +148,111 @@ class RedisConf(object):
     def __str__(self):
         return self.assemble()
 
-def which(binary_name, pathvar=None):
-    """ Deduces the path corresponding to an executable name,
-        as per the UNIX command `which`. Optionally takes an
-        override for the $PATH environment variable.
-        Always returns a string - an empty one for those
-        executables that cannot be found.
-    """
-    return find_executable(binary_name, pathvar or which.pathvar) or ""
-
-which.pathvar = os.environ.get('PATH')
-
 def redis_server(*args):
     return asyncio.create_subprocess_exec(
         which('redis-server'), *args,
         stdout=asyncio.subprocess.DEVNULL,
         stderr=asyncio.subprocess.DEVNULL)
 
-async def run_redis_server():
-    print("[daemon] Starting Redis…")
-    daemon = await redis_server()
+async def run_redis(*args):
+    logging.debug("[daemon] Starting Redis…")
+    daemon = await redis_server(*args)
     
-    print(f"[daemon] PID = {daemon.pid}")
+    logging.debug(f"[daemon] PID = {daemon.pid}")
     PID.set(daemon.pid)
     
+    logging.debug("[daemon] Running Redis subprocess…")
     try:
-        print("[daemon] Awaiting interrupt signal")
         await daemon.wait()
-    except KeyboardInterrupt:
-        print("[daemon] Terminating process…")
+    except asyncio.CancelledError:
+        logging.debug("")
+        logging.debug("[daemon] Terminating process…")
         daemon.terminate()
     
     if daemon.returncode is None:
-        print("[daemon] Killing process…")
+        logging.debug("[daemon] Killing process…")
         daemon.kill()
     
-    print(f"[daemon] RETVAL = {daemon.returncode}")
+    logging.debug(f"[daemon] RETVAL = {daemon.returncode}")
+    return daemon
 
-async def run_redis_task():
-    PID.set(-1)
-    task = asyncio.create_task(run_redis_server())
-    taskID = hex(id(task))
-    pid = PID.get()
-    print(f"[runctx] Redis server task created <id:{taskID}>")
-    print(f"[runctx] PID = {pid}")
-    return await task
+class RedRun(object):
+    
+    def __init__(self, confpath):
+        path = os.fspath(confpath)
+        if not os.path.exists(path):
+            raise ValueError("bad Redis configuration file path")
+        self.loop = asyncio.get_event_loop()
+        self.path = path
+        if DEBUG:
+            self.loop.set_debug(DEBUG)
+    
+    def run(self):
+        self.loop.run_until_complete(self.task)
+    
+    def __enter__(self):
+        self.task = self.loop.create_task(
+                                run_redis(self.path))
+        self.loop.add_signal_handler(signal.SIGINT,  self.task.cancel)
+        self.loop.add_signal_handler(signal.SIGTERM, self.task.cancel)
+        return self
+    
+    def __exit__(self, exc_type=None, exc_val=None, exc_tb=None):
+        self.loop.close()
+        return exc_type is None
 
-def schedule(target, *, loop=None):
-    if asyncio.iscoroutine(target):
-        return asyncio.ensure_future(target, loop=loop)
-    typename = nameof(type(target))
-    raise TypeError(f"target must be a coroutine (not {typename})")
-
-def main():
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
+def test_redis_conf():
+    
+    loop = asyncio.get_event_loop()
+    
+    with RedisConf() as redisconf:
+        
+        assert redisconf.file.exists
+        
+        print("REDIS CONF: full text")
+        print('*' * 100)
+        print(str(redisconf))
+        print('*' * 100)
+        print("REDIS CONF: multidict")
+        print('*' * 100)
+        pprint(redisconf.config)
+        print('*' * 100)
+        
+        # ctx = contextvars.copy_context()
+        # ctx.run(main, redisconf.file.name)
+        # pid = ctx[PID]
+        # print("[runctx] PID = {pid}")
+        # if pid > 0:
+        #     print("[runctx] Sending interrupt to pid #{pid}…")
+        #     os.kill(pid, signal.SIGINT)
+        
+        # with warnings.catch_warnings():
+        #     warnings.simplefilter("ignore")
+        #     try:
+        #         task = asyncio.run(run_redis(redisconf.file.name))
+        #     except KeyboardInterrupt:
+        #         print()
+        #         print("[runctx] Shutting down…")
+        # task = asyncio.run(run_redis(redisconf.file.name))
+        
+        # task = asyncio.create_task(run_redis(redisconf.file.name))
+        task = loop.create_task(run_redis(redisconf.file.name))
+        loop.add_signal_handler(signal.SIGINT,  task.cancel)
+        loop.add_signal_handler(signal.SIGTERM, task.cancel)
+        
         try:
-            asyncio.run(run_redis_task())
-        except KeyboardInterrupt:
-            print()
-            print("[runctx] Shutting down…")
+            loop.run_until_complete(task)
+        finally:
+            loop.close()
+        
+    assert not hasattr(redisconf, 'file')
 
-async def async_background():
-    loop = asyncio.new_event_loop()
-    with concurrent.futures.ProcessPoolExecutor() as pool:
-        result = await loop.run_in_executor(pool, run_redis_task())
-    print('custom process pool', result)
-
-def background():
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        future = executor.submit(main)
-        print(future.result)
+def test_redrun():
+    
+    with RedisConf() as settings:
+        with RedRun(settings.path) as redrunner:
+            redrunner.run()
 
 if __name__ == '__main__':
-    # main()
-    # asyncio.run(background())
-    background()
-    print("YO DOGG")
+    # test_redis_conf()
+    test_redrun()
